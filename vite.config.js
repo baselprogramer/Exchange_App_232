b481d7839025db6ba2c5e813d1de57eb24a25bf1
baselprogramer <prencbasel2000@gmail.com>
2026-05-07 14:46:17 +0300

    Final Edit vite.config edit LAZY LOAD

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,16 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    allowedHosts: [
-      'hypoeutectic-unteamed-gale.ngrok-free.dev'
-    ]
-  }
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        },
+      },
+    },
+    chunkSizeWarningLimit: 800, 
+  },
 })
